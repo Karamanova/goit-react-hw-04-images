898be0939f02686ca593916e736d2cb7dc4def05
Maryna Karamanova <karamanova91@gmail.com>
2023-03-02 15:41:53 +0200

    toast messages optimized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import * as API from 'services/Pixabay.Api';
-import { toast } from 'react-toastify';
 import { Button } from 'components/Button/Button';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Loader } from 'components/Loader/Loader';
@@ -9,12 +8,14 @@ import { Modal } from 'components/Modal/Modal';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { SearchError } from 'components/SearchError/SearchError';
 import { Container } from './App.styled';
+
 const Status = {
   IDLE: 'idle',
   PENDING: 'pending',
   RESOLVED: 'resolved',
   REJECTED: 'rejected',
 };
+
 export class App extends Component {
   state = {
     query: '',
@@ -28,85 +29,83 @@ export class App extends Component {
     largeImageURL: '',
     tags: '',
   };
-  async componentDidUpdate (prevProps, prevState) {
-  const { query, page } = this.state;
-  const {query: prevQuery, page: prevPage} = prevState;
-   if(prevQuery !== this.state.query || prevPage !== this.state.page){
-    API.searchParams.q = query;
-    API.searchParams.page = page;
-    this.setState({status: Status.PENDING})
-    try {
-      const { totalHits, hits } = await API.getImages(API.searchParams);
-        if(totalHits || hits.length){
-          if (page === 1) {toast.success(`🦄 We found ${totalHits} images.`);};
-          if (page >= 1) {
-            this.setState((prevState) => ({
-            totalHits: totalHits,
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    const { query: prevQuery, page: prevPage } = prevState;
+
+    if (prevQuery !== query || prevPage !== page) {
+      API.searchParams.q = query;
+      API.searchParams.page = page;
+      this.setState({ status: Status.PENDING });
+
+      try {
+        const { totalHits, hits } = await API.getImages(API.searchParams);
+
+        if (hits.length > 0) {
+          this.setState((prevState) => ({
+            totalHits,
             hits: [...prevState.hits, ...hits],
             status: Status.RESOLVED,
-            }));
-          };
-          if(hits.length < 12){toast.info(`🦄 No more images for ${query}`);};
+          }));
+          if (page === 1)
+            { toast.success(`🦄 We found ${totalHits} images.`) };
+          if (hits.length < 12)
+            { toast.info(`🦄 No more images for ${query}`);
+          }
+        } else {
+          toast.error('🦄 Sorry, there are no images matching your search query. Please try again.');
+          this.setState({ status: Status.REJECTED, });
         }
-        else {
-          this.setState ({
-          totalHits: null,
-          hits: [],
-          status: Status.REJECTED,
-          });
-          toast.error("🦄 Sorry, there are no images matching your search query. Please try again.");
-        };
-    } catch (error) {
-      this.setState({
-      totalHits: null,
-      hits: [],
-      status: Status.REJECTED,
-      error,
-      });
-      toast.info(`Something went wrong ${error}`);
-      } 
-    };
+      } catch (error) {
+        toast.info(`🦄 Something went wrong ${error}`);
+        this.setState({ status: Status.REJECTED });
+      }
+    }
+
     if (prevState.hits !== this.state.hits) {
       window.scrollBy({
         top: document.body.scrollHeight,
         behavior: 'smooth',
       });
-    };
+    }
+  }
+
+  handleFormSearch = (query) => {
+    const trimmedQuery = query.trim();
+    this.setState({
+      query: trimmedQuery,
+      page: 1,
+      totalHits: null,
+      hits: [],
+    });
   };
 
-handleFormSearch = (query) => {
-  const trimmedQuery = query.trim();
-  this.setState({ 
-  query: trimmedQuery,
-  page: 1,
-  totalHits: null,
-  hits: [],
-  });
-};
+  handleClickLoadMore = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
+  };
 
-handleClickLoadMore = () => this.setState(({page}) => ({ page: page + 1 }));
-
-handleToggleModal = (e) => {
-  this.setState(({showModal}) => ({ showModal: !showModal }));
-  if (!this.state.showModal) {
-    this.setState({ 
-      largeImageURL: e.target.dataset.source, 
-      tags: e.target.alt 
-    });
-  }
-};
+  handleToggleModal = (e) => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+    if (!this.state.showModal) {
+      this.setState({
+        largeImageURL: e.target.dataset.source,
+        tags: e.target.alt,
+      });
+    }
+  };
 
   render() {
     const { hits, showModal, largeImageURL, tags, status } = this.state;
+
     return (
-    <Container>
-      <Searchbar onSearch={this.handleFormSearch}/>
-        <ToastContainer autoClose={3000} hideProgressBar={true} checkmark={false} />
-      {status === 'rejected' && <SearchError/>}
-      {status === 'pending' && <Loader/>}
-      {hits.length !== 0 && <ImageGallery images={hits} onClick={this.handleToggleModal}/>}
-      {hits.length >= API.searchParams.per_page &&
-        <Button onClick={this.handleClickLoadMore}/>}
+      <Container>
+        <Searchbar onSearch={this.handleFormSearch} />
+        <ToastContainer autoClose={3000} hideProgressBar checkmark={false} />
+        {status === Status.REJECTED && <SearchError />}
+        {status === Status.PENDING && <Loader />}
+        {hits.length !== 0 && <ImageGallery images={hits} onClick={this.handleToggleModal} />}
+        {hits.length >= API.searchParams.per_page && <Button onClick={this.handleClickLoadMore}/>}
       {showModal && 
         <Modal onClose={this.handleToggleModal}>
           <img src={largeImageURL} alt={tags}/>
